24d4d12e6db97ae617695fe5e693a407b5e89a3c
amangit007 <65654655+amangit007@users.noreply.github.com>
2021-08-13 15:49:46 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn =sqlite3.connect('weather.db')
-conn.execute('insert into city (name) values ("kalka")')
-conn.commit()
-result=conn.execute('SELECT * FROM city')
-for i in result:
-    print(i)
